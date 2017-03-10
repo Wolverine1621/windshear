eadb4abfdfea47bcc2d00098f276e3edb102a5f5
Noah <noahk316@gmail.com>
2017-03-10 22:37:45 +0000

    Added UTC clock and favicon

---
+++ js/clock.js
@@ -0,0 +1,38 @@
+var h, m, s;
+var timeString;
+
+window.onload = init;
+
+function init() {
+    startTime();
+}
+
+function startTime() {
+    var today = new Date ();
+    
+    getUTCTime(today);
+    
+    h = checkTime(h);
+    m = checkTime(m);
+    s = checkTime(s)
+    
+    timeString = "" + h + ":" + m + ":" + s + " UTC";
+    
+    document.getElementById("time").innerHTML = timeString;
+    
+    setTimeout(startTime, 500);
+}
+
+function getUTCTime(today) {
+    h = today.getUTCHours();
+    m = today.getUTCMinutes();
+    s = today.getUTCSeconds();
+}
+
+function checkTime(i) {
+  if (i < 10) {
+      i = "0" + i;
+  } // add zero in front of numbers < 10
+
+  return i;
+}
